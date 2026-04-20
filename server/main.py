d7c4a3b90aecbbf06f561baea19a782e53d6aa60
José Ramírez <josecatun2015@hotmail.com>
2026-04-20 11:32:04 -0600

    feat: initialize Angular project structure and server environment configuration

---
+++ server/main.py
@@ -0,0 +1,26 @@
+import os
+from flask import Flask, jsonify
+from flask_cors import CORS
+from dotenv import load_dotenv
+
+load_dotenv();
+
+def create_app():
+    app = Flask(__name__)
+    CORS(app, resources={r"/*": { "origins": "http://localhost:4200" }})
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    @app.route('/', methods=['GET'])
+    def hello_world():
+        return jsonify({
+            "status": "success",
+            "message": "Inventory Web API - Online",
+            "version": "1.0.0"
+        })
+
+    return app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(host="0.0.0.0", port=4200, debug=True)
